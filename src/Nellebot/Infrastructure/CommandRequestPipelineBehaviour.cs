23e2b0920850497f0f402f1d18382bde4639adfe
Bob Loblaw <iwrite@selfdocumentingco.de>
2025-02-16 19:50:08 +0100

    Update CommandRequestPipelineBehaviour to handle failed interactions

--- src/Nellebot/Infrastructure/CommandRequestPipelineBehaviour.cs
+++ src/Nellebot/Infrastructure/CommandRequestPipelineBehaviour.cs
@@ -2,10 +2,13 @@
 using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus.Commands;
+using DSharpPlus.Commands.Processors.SlashCommands;
+using DSharpPlus.Entities;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Nellebot.CommandHandlers;
 using Nellebot.Services.Loggers;
+using Nellebot.Utils;
 
 namespace Nellebot.Infrastructure;
 
@@ -32,31 +35,95 @@ public class CommandRequestPipelineBehaviour<TRequest, TResponse> : IPipelineBeh
         {
             return await next().ConfigureAwait(false);
         }
+        catch (InteractionException ex) when (request is BotCommandCommand commandCommand)
+        {
+            await HandleInteractionException(commandCommand, ex);
+            return default!;
+        }
+        catch (Exception ex) when (request is BotSlashCommand slashCommand)
+        {
+            await HandleSlashCommandException(slashCommand, ex);
+            return default!;
+        }
         catch (Exception ex) when (request is BotCommandCommand commandCommand)
         {
             await HandeCommandCommandException(commandCommand, ex);
             return default!;
         }
-        catch (Exception ex) when (request is IRequest command)
+        catch (Exception ex) when (request is IRequest)
         {
-            HandleRequestException(command, ex);
+            HandleRequestException(ex);
             return default!;
         }
     }
 
-    private void HandleRequestException(IRequest request, Exception ex)
+    private void HandleRequestException(Exception ex)
     {
         _discordErrorLogger.LogError(ex.Message);
 
         _logger.LogError(ex, nameof(HandleRequestException));
     }
 
+    private async Task HandleInteractionException(BotCommandCommand command, InteractionException ex)
+    {
+        if (ex.Interaction.ResponseState is DiscordInteractionResponseState.Unacknowledged)
+        {
+            DiscordInteractionResponseBuilder interactionResponse =
+                new DiscordInteractionResponseBuilder().WithContent(ex.Message).AsEphemeral();
+            await ex.Interaction.CreateResponseAsync(
+                DiscordInteractionResponseType.ChannelMessageWithSource,
+                interactionResponse);
+        }
+        else
+        {
+            DiscordFollowupMessageBuilder followupResponse =
+                new DiscordFollowupMessageBuilder().WithContent(ex.Message).AsEphemeral();
+            await ex.Interaction.CreateFollowupMessageAsync(followupResponse);
+        }
+
+        _discordErrorLogger.LogCommandError(command.Ctx, ex.ToString());
+
+        _logger.LogError(ex, nameof(HandleInteractionException));
+    }
+
+    private async Task HandleSlashCommandException(BotSlashCommand request, Exception ex)
+    {
+        SlashCommandContext ctx = request.Ctx;
+
+        if (ctx.Interaction.ResponseState is DiscordInteractionResponseState.Unacknowledged)
+        {
+            await ctx.RespondAsync(ex.Message, true);
+        }
+        else
+        {
+            await ctx.FollowupAsync(ex.Message, true);
+        }
+
+        _discordErrorLogger.LogCommandError(ctx, ex.ToString());
+
+        _logger.LogError(ex, nameof(HandleSlashCommandException));
+    }
+
     private async Task HandeCommandCommandException(BotCommandCommand request, Exception ex)
     {
-        // TODO respond with ephemeral message if possible
         CommandContext ctx = request.Ctx;
 
-        await ctx.RespondAsync(ex.Message);
+        if (ctx is SlashCommandContext slashCtx)
+        {
+            if (slashCtx.Interaction.ResponseState is DiscordInteractionResponseState.Unacknowledged)
+            {
+                await slashCtx.RespondAsync(ex.Message);
+            }
+            else
+            {
+                await slashCtx.FollowupAsync(ex.Message, true);
+            }
+        }
+        else
+        {
+            await ctx.RespondAsync(ex.Message);
+        }
+
 
         _discordErrorLogger.LogCommandError(ctx, ex.ToString());
 
