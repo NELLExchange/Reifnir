973aace31879620d88f3c9960c3744f89862f531
Bob Loblaw <iwrite@selfdocumentingco.de>
2025-05-16 23:19:27 +0200

    Allow on sunrise

--- src/Nellebot/NotificationHandlers/Seventeen.cs
+++ src/Nellebot/NotificationHandlers/Seventeen.cs
@@ -7,6 +7,19 @@ public static class Seventeen
 {
     public static bool IsMatch(string input)
     {
+        var sunrise = new DateTimeOffset(
+            year: 2025,
+            month: 5,
+            day: 17,
+            hour: 4,
+            minute: 35,
+            second: 0,
+            TimeSpan.FromHours(2));
+
+        DateTimeOffset currentTime = DateTimeOffset.UtcNow;
+
+        if (currentTime >= sunrise) return false;
+
         const string numberString = "1705";
 
         // Remove all non-digits from input
