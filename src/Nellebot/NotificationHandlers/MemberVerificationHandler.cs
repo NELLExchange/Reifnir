03ae60d9a979a5f7017d7454ccaf8f577c22f4eb
Bob Loblaw <iwrite@selfdocumentingco.de>
2025-09-19 22:04:01 +0200

    Wait and check for quarantine before approving new members

--- src/Nellebot/NotificationHandlers/MemberVerificationHandler.cs
+++ src/Nellebot/NotificationHandlers/MemberVerificationHandler.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using MediatR;
+using Microsoft.Extensions.Options;
 using Nellebot.Services;
 using Nellebot.Utils;
 using Nellebot.Workers;
@@ -13,18 +15,24 @@ namespace Nellebot.NotificationHandlers;
 
 public class MemberVerificationHandler : INotificationHandler<GuildMemberAddedNotification>
 {
+    private const int ApproveDelayDurationSeconds = 10;
+    private const int SuspiciousAccountAgeThresholdDays = 2;
+
     private readonly DiscordResolver _discordResolver;
     private readonly EventQueueChannel _eventQueueChannel;
     private readonly QuarantineService _quarantineService;
+    private readonly BotOptions _botOptions;
 
     public MemberVerificationHandler(
         DiscordResolver discordResolver,
         EventQueueChannel eventQueueChannel,
-        QuarantineService quarantineService)
+        QuarantineService quarantineService,
+        IOptions<BotOptions> botOptions)
     {
         _discordResolver = discordResolver;
         _eventQueueChannel = eventQueueChannel;
         _quarantineService = quarantineService;
+        _botOptions = botOptions.Value;
     }
 
     public async Task Handle(GuildMemberAddedNotification notification, CancellationToken cancellationToken)
@@ -33,18 +41,34 @@ public class MemberVerificationHandler : INotificationHandler<GuildMemberAddedNo
         DiscordMember member = args.Member;
 
         // Check if user needs to be quarantined for having a brand-new account
-        const int suspiciousAccountAgeThresholdDays = 2;
         TimeSpan memberAccountAge = DateTimeOffset.UtcNow - member.Id.GetSnowflakeTime();
 
         DiscordMember botMember = _discordResolver.GetBotMember();
 
-        if (memberAccountAge < TimeSpan.FromDays(suspiciousAccountAgeThresholdDays))
+        if (memberAccountAge < TimeSpan.FromDays(SuspiciousAccountAgeThresholdDays))
         {
-            var quarantineReason = $"User account age under threshold (< {suspiciousAccountAgeThresholdDays} days)";
+            var quarantineReason = $"User account is less than {SuspiciousAccountAgeThresholdDays} days old.";
             await _quarantineService.QuarantineMember(member, botMember, quarantineReason);
         }
         else
         {
+            // Wait for any post-join quarantine actions
+            await Task.Delay(TimeSpan.FromSeconds(ApproveDelayDurationSeconds), cancellationToken);
+
+            DiscordMember? updatedMember = await _discordResolver.ResolveGuildMember(member.Id);
+
+            // If the member is null for some reason, approve them anyway
+            if (updatedMember is not null)
+            {
+                bool memberIsQuarantined = member.Roles.Any(r => r.Id == _botOptions.QuarantineRoleId);
+
+                if (memberIsQuarantined)
+                {
+                    // No greet for you!
+                    return;
+                }
+            }
+
             await _eventQueueChannel.Writer.WriteAsync(
                 new MemberApprovedNotification(member, botMember),
                 cancellationToken);
