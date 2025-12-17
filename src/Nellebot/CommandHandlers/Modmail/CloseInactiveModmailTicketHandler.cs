b75809b92baf078abb1541f44bdf5e6cb66340c2
Bob Loblaw <iwrite@selfdocumentingco.de>
2025-12-17 21:10:52 +0100

    Log a warning and close inactive ticket if ticket thread is not found

--- src/Nellebot/CommandHandlers/Modmail/CloseInactiveModmailTicketHandler.cs
+++ src/Nellebot/CommandHandlers/Modmail/CloseInactiveModmailTicketHandler.cs
@@ -5,6 +5,7 @@ using DSharpPlus.Entities;
 using MediatR;
 using Nellebot.Common.Models.Modmail;
 using Nellebot.Data.Repositories;
+using Nellebot.Services.Loggers;
 using Nellebot.Utils;
 
 namespace Nellebot.CommandHandlers.Modmail;
@@ -12,12 +13,17 @@ namespace Nellebot.CommandHandlers.Modmail;
 public class CloseInactiveModmailTicketHandler : IRequestHandler<CloseInactiveModmailTicketCommand>
 {
     private readonly ModmailTicketRepository _modmailTicketRepo;
+    private readonly IDiscordErrorLogger _discordErrorLogger;
     private readonly DiscordResolver _resolver;
 
-    public CloseInactiveModmailTicketHandler(DiscordResolver resolver, ModmailTicketRepository modmailTicketRepo)
+    public CloseInactiveModmailTicketHandler(
+        DiscordResolver resolver,
+        ModmailTicketRepository modmailTicketRepo,
+        IDiscordErrorLogger discordErrorLogger)
     {
         _resolver = resolver;
         _modmailTicketRepo = modmailTicketRepo;
+        _discordErrorLogger = discordErrorLogger;
     }
 
     public async Task Handle(CloseInactiveModmailTicketCommand request, CancellationToken cancellationToken)
@@ -27,13 +33,21 @@ public class CloseInactiveModmailTicketHandler : IRequestHandler<CloseInactiveMo
         ModmailTicketPost ticketPost = ticket.TicketPost
                                        ?? throw new Exception("The ticket does not have a post channelId");
 
-        DiscordThreadChannel threadChannel = _resolver.ResolveThread(ticketPost.ChannelThreadId)
-                                             ?? throw new Exception("Could not resolve thread channel");
-
         await _modmailTicketRepo.CloseTicket(ticket, cancellationToken);
 
-        const string ticketClosureMessage = "This ticket has been closed due to inactivity.";
+        DiscordThreadChannel? threadChannel = _resolver.ResolveThread(ticketPost.ChannelThreadId);
 
-        await threadChannel.SendMessageAsync(ticketClosureMessage);
+        if (threadChannel is null)
+        {
+            _discordErrorLogger.LogWarning(
+                $"Could not resolve thread channel for ticket id: {ticket.Id}",
+                $"Thread channel for ticket id: {ticket.Id} could not be resolved. Probably deleted. Closing ticket anyway.");
+        }
+        else
+        {
+            const string ticketClosureMessage = "This ticket has been closed due to inactivity.";
+
+            await threadChannel.SendMessageAsync(ticketClosureMessage);
+        }
     }
 }
